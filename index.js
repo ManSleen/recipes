5f3b260b03b10725bdd431933ce9c45fbfba7014
Mike VanSleen <mike.vansleen@gmail.com>
2019-08-08 11:06:23 -0600

    added port env variable

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const server = require("./api/server.js");
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 server.listen(port, () => {
   console.log(`API up and running on port ${port}`);
 });
